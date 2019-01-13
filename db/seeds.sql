4afad2c78085a58c3dc4e3f8bee0c0f0d9187ac2
Lori Salazar <loriatuci@gmail.com>
2019-01-12 23:04:50 -0800

    first code set

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Messyburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spiceyburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smashburger', false);
